c3cf4f63198327522af5e1c6cee3cc61031e543f
Neal DeBuhr <ndebuhr@protonmail.com>
2017-05-16 07:02:04 -0400

    Add basics of solution generation system

--- stress_strain.c
+++ stress_strain.c
@@ -12,8 +12,9 @@ stress-strain element.
 #define NUM_VARS 14
 
 void assign_vals(double * var_ptrs[NUM_VARS], char unknown, char vars[NUM_VARS][2][255]);
-static double * make_constitutive(double E, double v);
-
+void populate_stiffness(double E, double v, double * stiffness);
+int get_unknown_ind(double * var_ptrs[NUM_VARS], char unknown);
+  
 int main (void)
 {
   // Programming variables
@@ -23,6 +24,7 @@ int main (void)
 			       {"J","Shear Strain in XY"},{"K","Shear Strain in YZ"},{"L","Shear Strain in XZ"},
 			       {"M","Modulus of Elasticity"},{"N","Poisson's Ratio"}};
   char unknown;
+  int unknown_ind;
   int i;
   // Solid mechanics variables
   double sigma_x, sigma_y, sigma_z, tau_xy, tau_yz, tau_xz, epsilon_x, epsilon_y, epsilon_z, gamma_xy, gamma_yz, gamma_xz, E, v;
@@ -40,23 +42,15 @@ int main (void)
   unknown=getchar();
   while (getchar()!='\n'); //clear residual stdin buffer chars
 
+  unknown_ind=get_unknown_ind(var_ptrs,unknown);
   assign_vals(var_ptrs,unknown,vars);
-  
-  switch (unknown) {
-  case 'A' :
-    break;
-  case 'B' :
-    break;
-  case 'C' :
-    break;
-  case 'D' :
-    break;
-  case 'E' :
-    break;
-  case 'F' :
-    break;
-  default :
-    break;
+
+  for (i=0; i<1000000; i++); //guess, no check
+  {
+    var_ptrs[unknown_ind]=((double)rand()/(double)RAND_MAX); //not uniform, but okay
+    // TODO Finish loop and fix 0-1 rand to proper range based on variable type
+    double stiffness[6][6];
+    populate_stiffness(E,v,stiffness);
   }
   
   return 0;
@@ -77,18 +71,17 @@ void assign_vals(double * var_ptrs[NUM_VARS], char unknown, char vars[NUM_VARS][
     }
 }
 
-static double * make_constitutive(double E, double v)
+void populate_stiffness(double E, double v, double * stiffness)
 {
   int i;
   int j;
   double factor;
-  static double constitutive[3][3];
 
-  constitutive[0][0]=1-v;
-  constitutive[1][0]=v;
-  constitutive[0][1]=v;
-  constitutive[1][1]=1-v;
-  constitutive[2][2]=0.5-v;
+  stiffness[0][0]=1-v;
+  stiffness[1][0]=v;
+  stiffness[0][1]=v;
+  stiffness[1][1]=1-v;
+  stiffness[2][2]=0.5-v;
   //leaves four elements at 0, as intended
   
   factor=E/((1+v)*(1-2*v));
@@ -96,8 +89,19 @@ static double * make_constitutive(double E, double v)
     {
       for (j=0; j<3; j++)
 	{
-	  constitutive[i][j]=constitutive[i][j]*factor;
+	  stiffness[i][j]=stiffness[i][j]*factor;
 	}
     }
-  return constitutive;
+  return stiffness;
+}
+
+int get_unknown_ind(double * var_ptrs[NUM_VARS], char unknown)
+{
+  int i;
+
+  for (i=0; i<NUM_VARS; i++)
+    {
+      if (unknown == vars[i][0][0])
+	return i;
+    }
 }
