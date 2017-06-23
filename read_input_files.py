23b8b199a9bb03f42aa7e75a39252195b4cc77b1
Neal DeBuhr <ndebuhr@protonmail.com>
2017-06-23 17:53:41 -0400

    Create csv parsing program

---
+++ read_input_files.py
@@ -0,0 +1,23 @@
+from utils import write_csv_rows, read_csv_rows
+
+class output_table:
+    def __init__(self, filename, content=None):
+        self.filename = filename
+
+connect_tbl=output_table('connectivity.csv')
+force_tbl=output_table('forces.csv')
+bc_tbl=output_table('boundary_conditions.csv')
+sim_tbl=output_table('simulation_parameters.csv')
+
+output_files=[connect_tbl,force_tbl,bc_tbl,sim_tbl]
+
+# Read in content and convert to nested list
+for i in range(0,len(output_files)):
+    output_files[i].content = read_csv_rows(output_files[i].filename)
+    for j in range(0,len(output_files[i].content)):
+        output_files[i].content[j]=output_files[i].content[j].split(',')
+    print('Successfully read ' + output_files[i].filename)
+
+for i in range(0,len(output_files)):
+    print(output_files[i].filename)
+    print(output_files[i].content)
