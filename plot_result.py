d0a35424ae884aff06fc0be58198a8c6ae0fad3f
Neal DeBuhr <ndebuhr@protonmail.com>
2017-06-27 14:07:58 -0400

    Create system to display truss analysis results

---
+++ plot_result.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import truss_struct as ts
+
+def get_max_stress(trusses):
+    max_stress = trusses[0].stress
+    for i in range(0,len(trusses)):
+        if (abs(trusses[i].stress) > max_stress):
+            max_stress = abs(trusses[i].stress)
+    return max_stress
+
+def get_line_color(stress, max_stress):
+    perc_of_max = abs(stress)/abs(max_stress)
+    return [perc_of_max,0,0]
+
+u, stresses, R, trusses = ts.calc_solution()
+
+max_stress = get_max_stress(trusses)
+plt.figure(1)
+
+for i in range(0,len(trusses)):
+    x1 = trusses[i].x1
+    x2 = trusses[i].x2
+    y1 = trusses[i].y1
+    y2 = trusses[i].y2
+    stress = trusses[i].stress
+    line_color = get_line_color(stress,max_stress)
+    plt.plot([x1, x2], [y1, y2], color=line_color)
+    plt.text((x1+x2)/2, (y1+y2)/2, r'$\sigma='+('%.4E' %stress )+'$')
+    # ax.text(3, 2, u'
+
+plt.title('Truss Stresses')
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
--- truss_struct.py
+++ truss_struct.py
@@ -67,41 +67,45 @@ def assign_stresses(u, trusses):
         truss.stress = stress.tolist()[0][0]
         truss.strain = truss.stress/truss.E
 
-# dof = int(input('Number of nodes: '))*spatial_dims
-dof = sim_params[1].val
+def calc_solution():
+        
+    # dof = int(input('Number of nodes: '))*spatial_dims
+    dof = sim_params[1].val
 
-# A = truss(x1=0.5,x2=0,y1=0.3,y2=0.3,
-#           E=70e9,A=200*1e-6,node1=0,node2=1)
-# B = truss(x1=0.5,x2=0.9,y1=0.3,y2=0,
-#           E=70e9,A=200*1e-6,node1=0,node2=2)
-# trusses = [A,B]
+    # A = truss(x1=0.5,x2=0,y1=0.3,y2=0.3,
+    #           E=70e9,A=200*1e-6,node1=0,node2=1)
+    # B = truss(x1=0.5,x2=0.9,y1=0.3,y2=0,
+    #           E=70e9,A=200*1e-6,node1=0,node2=2)
+    # trusses = [A,B]
 
-# f1 = force(fx=0,fy=-12e3,node=0)
-# forces = [f1]
+    # f1 = force(fx=0,fy=-12e3,node=0)
+    # forces = [f1]
+    
+    # n1 = fixed_node(1,'x',0)
+    # n2 = fixed_node(1,'y',0)
+    # n3 = fixed_node(2,'x',0)
+    # n4 = fixed_node(2,'y',0)
+    # fixed_nodes=[n1,n2,n3,n4]
+    
+    K = compile_K(dof,trusses)
+    F = compile_F(dof,forces)
+    
+    c = set_c(K,numerical_mult)
+    K, F = fix_nodes(K,F,c,fixed_nodes)
+    u = solve_u(K,F)
+    assign_stresses(u,trusses)
+    
+    K_orig = compile_K(dof,trusses)
+    F_orig = compile_F(dof,forces)
+    R = np.matrix(K_orig)*np.matrix(u)-np.matrix(F_orig)
+    R = R.tolist()
+    
+    stresses=[]
+    for i in range(0, len(trusses)):
+        stresses.append(trusses[i].stress)
+        
+    print('\nDisplacements\n',np.matrix(u))
+    print('\nStresses\n',np.matrix(stresses))
+    print('\nReactions\n',np.matrix(R))
 
-# n1 = fixed_node(1,'x',0)
-# n2 = fixed_node(1,'y',0)
-# n3 = fixed_node(2,'x',0)
-# n4 = fixed_node(2,'y',0)
-# fixed_nodes=[n1,n2,n3,n4]
-
-K = compile_K(dof,trusses)
-F = compile_F(dof,forces)
-
-c = set_c(K,numerical_mult)
-K, F = fix_nodes(K,F,c,fixed_nodes)
-u = solve_u(K,F)
-assign_stresses(u,trusses)
-
-K_orig = compile_K(dof,trusses)
-F_orig = compile_F(dof,forces)
-R = np.matrix(K_orig)*np.matrix(u)-np.matrix(F_orig)
-R = R.tolist()
-
-stresses=[]
-for i in range(0, len(trusses)):
-    stresses.append(trusses[i].stress)
-
-print('\nDisplacements\n',np.matrix(u))
-print('\nStresses\n',np.matrix(stresses))
-print('\nReactions\n',np.matrix(R))
+    return u, stresses, R, trusses
